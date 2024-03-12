8c4cd23ab7ba42dc8d9383e4ff2785e8504f4bab
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2024-03-12 17:46:41 +0200

    Add 'ScheduledService' which get weather data from ilmateenistus.ee ever specified time and save it to the database

---
+++ src/main/java/com/artjomkuznetsov/deliveryfee/services/ScheduledService.java
@@ -0,0 +1,80 @@
+package com.artjomkuznetsov.deliveryfee.services;
+
+import com.artjomkuznetsov.deliveryfee.models.WeatherData;
+import com.artjomkuznetsov.deliveryfee.repositories.WeatherDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@EnableScheduling
+public class ScheduledService {
+    public static final String ESTONIAN_WEATHER_API_URL = "https://www.ilmateenistus.ee/ilma_andmed/xml/observations.php";
+    public static final  List<String> STATIONS = List.of("Tallinn-Harku", "Tartu-Tõravere", "Pärnu");
+
+    private final RestTemplate restTemplate;
+    private final WeatherDataRepository weatherDataRepository;
+
+    @Autowired
+    public ScheduledService(RestTemplate restTemplate, WeatherDataRepository weatherDataRepository) {
+        this.restTemplate = restTemplate;
+        this.weatherDataRepository = weatherDataRepository;
+    }
+
+    //@Scheduled(cron = "0 15 * * * *")
+    @Scheduled(cron = "*/5 * * * * *")
+    public void getWeatherDataFromAPI() {
+        String responseData = restTemplate.getForObject(ESTONIAN_WEATHER_API_URL, String.class);
+        if (responseData != null) {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            try {
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                ByteArrayInputStream input = new ByteArrayInputStream(
+                        responseData.getBytes(StandardCharsets.UTF_8));
+
+                Document doc = builder.parse(input);
+                NodeList nodeList = doc.getElementsByTagName("station");
+                for (int i = 0; i < nodeList.getLength(); i++) {
+                    Node node = nodeList.item(i);
+
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        Element element = (Element) node;
+                        String stationName = element.getElementsByTagName("name").item(0).getTextContent();
+                        if (STATIONS.contains(stationName)) {
+                            WeatherData weatherData = new WeatherData();
+                            weatherData.setStation(stationName);
+                            weatherData.setStationWMO(element.getElementsByTagName("wmocode").item(0).getTextContent());
+                            weatherData.setAirTemperature(Float.parseFloat(element.getElementsByTagName("airtemperature").item(0).getTextContent()));
+                            weatherData.setWeatherPhenomenon(element.getElementsByTagName("phenomenon").item(0).getTextContent());
+                            weatherData.setWindSpeed(Float.parseFloat(element.getElementsByTagName("windspeed").item(0).getTextContent()));
+                            weatherData.setObservationTimestamp(LocalDateTime.now());
+                            weatherDataRepository.save(weatherData);
+                        }
+                    }
+                }
+            } catch (SAXException e) {
+                System.out.println("An error occurred while parsing the XML document with weather data. Please ensure that the XML document has the correct structure and try again.");
+            } catch (IOException e) {
+                System.out.println("An error occurred while processing weather data.");
+            } catch (ParserConfigurationException e) {
+                System.out.println("An error occurred while configuring the XML parser.");
+            }
+        }
+    }
+}

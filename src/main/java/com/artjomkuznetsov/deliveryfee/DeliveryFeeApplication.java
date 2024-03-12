b86aae84e07592b353e37ad039d4e6cf4704b432
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2024-03-12 16:51:51 +0200

    Init project

---
+++ src/main/java/com/artjomkuznetsov/deliveryfee/DeliveryFeeApplication.java
@@ -0,0 +1,13 @@
+package com.artjomkuznetsov.deliveryfee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryFeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryFeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artjomkuznetsov/deliveryfee/DeliveryFeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artjomkuznetsov.deliveryfee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryFeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
